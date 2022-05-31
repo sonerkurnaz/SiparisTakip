38cae7b7d1cea852627664146db1d5c44b2ccd7f
Soner Kurnaz <sonerkurnaz@outlook.com>
2022-05-31 14:03:47 +0300

    nortwind scaffold

---
+++ Northwind.Entity/Entity/CurrentProductList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Northwind.Entity.Entity
+{
+    public partial class CurrentProductList
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+    }
+}
---
+++ Northwind.Entity/Entity/ProductsAboveAveragePrice.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Northwind.Entity.Entity
+{
+    public partial class ProductsAboveAveragePrice
+    {
+        public string ProductName { get; set; }
+        public decimal? UnitPrice { get; set; }
+    }
+}
